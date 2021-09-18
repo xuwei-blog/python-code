8f9a21579e4db2dbb6f4d53ff7b2505651e4bd0d
vigman-xxxw <1319284583@qq.com>
2021-09-18 22:19:36 +0800

    turtle库--画五角星

---
+++ 9-18test1.py
@@ -0,0 +1,29 @@
+import turtle as t
+t.color('black','red')
+
+t.screensize(500,500,'yellow')
+t.begin_fill()
+
+
+
+for i in range(5):
+    t.left(145)
+    t.forward(100)
+
+
+
+
+t.end_fill()
+
+
+t.right(45)
+t.up()
+t.hideturtle()
+
+t.backward(-100)
+
+
+
+t.write('hello',font=('Arial',50,'normal'))
+
+
